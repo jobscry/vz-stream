b29f54614aa7b376f7a59f1aa0ef1955a9a5e44d
Joe Vasquez <joe.vasquez@gmail.com>
2010-01-07 23:01:27 -0500

    added admin tweaks

--- vz_stream/admin.py
+++ vz_stream/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 from models import Source, Entry
 
-admin.site.register(Source)
-admin.site.register(Entry)
+class SourceAdmin(admin.ModelAdmin):
+    date_hierarchy = 'last_modified'
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'url')
+        }),
+        ('Feed Options', {
+            'fields': ('feed_type', 'auto_link')
+        }),
+        ('Info', {
+            'fields': ('last_update_successful', 'etag', 'last_modified', 'last_status_code', 'error_message')
+        })
+    )
+    list_display = ('name', 'num_entries', 'feed_type', 'auto_link', 'last_status_code',
+        'last_modified', 'created_on', 'modified')
+    
+
+    def num_entries(self, obj):
+        return Entry.objects.filter(source=obj.pk).count()
+
+admin.site.register(Source, SourceAdmin)
+
+class EntryAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_on'
+    list_display = ('source', 'created_on', 'retrieved_on')
+
+admin.site.register(Entry, EntryAdmin)
