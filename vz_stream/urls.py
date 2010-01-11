918830a0beecf5464ba89a9e7ebe616272f01bcb
Joe Vasquez <joe.vasquez@gmail.com>
2010-01-11 17:14:35 -0500

    added basic stats view

--- vz_stream/urls.py
+++ vz_stream/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('vz_stream.views',
+    url(r'^stats/$', 'stream_stats', { 'pk':None }, "vz_stream:stream_stats"),
     (r'^view/(?P<pk>[0-9]+)/$', 'view_stream'),
     url(r'^view/$', 'view_stream', { 'pk':None }, "vz_stream:stream_view"),
     (r'^update/(?P<pk>[0-9]+)/$', 'update'),
--- vz_stream/views.py
+++ vz_stream/views.py
@@ -4,6 +4,41 @@ from models import Entry, Source
 
 NUM_ENTRIES = 20
 
+def stream_stats(request, pk=None):
+    """
+    Stream Stats
+    
+    total_entries: integer
+    entries_by_source:
+        {
+            source: num_entries,
+        }
+    
+    """
+    if pk is None:
+        sources = get_list_or_404(Source)
+    num_entries = float(Entry.objects.all().count())
+    sources_list = Source.objects.values_list('name', flat=True)
+    ebs_count = list()
+    ebs_percent = list()
+    
+    for source in sources:
+        count = Entry.objects.filter(source=source).count()
+        ebs_count.append(count)
+        ebs_percent.append(count/num_entries*100)
+
+    return render_to_response(
+        'vz_stream/stream_stats.html',
+        {
+            'num_entries': num_entries,
+            'ebs_count': ebs_count,
+            'ebs_percent': ebs_percent,
+            'sources_list': sources_list,
+        },
+        context_instance=RequestContext(request)
+    )
+
+
 def view_stream(request, pk):
     """
     View Stream
